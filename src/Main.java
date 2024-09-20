ca2936a7fe969c27507a5631ea4ae6ab50b11535
Jeffrey Balbalosa <31784615+jbalbalosa@users.noreply.github.com>
2024-09-20 13:13:03 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        
+//        Movie theMovie = new Adventure("Star Wars");
+//        Movie theMovie = Movie.getMovie("Science", "Star Wars");
+//        theMovie.watchMovie();
+
+        Scanner s = new Scanner(System.in);
+        while (true){
+            System.out.println("Enter Type (A for Adventure, C for Comedy, " +
+                    "S for Science Fiction, or Q for Quit):");
+            String type = s.nextLine();
+            if("Qq".contains((type))){
+                break; //quit the loop
+            }
+            System.out.println("Enter Movie Title");
+            String title = s.nextLine();
+            Movie movie = Movie.getMovie(type, title);
+            movie.watchMovie();
+        }
     }
 }
